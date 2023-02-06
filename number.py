7781bd366d71156d908d7261854106340125c70a
shiny <unique590284@gmail.com>
2023-02-06 16:52:31 +0800

    final

--- number.py
+++ number.py
@@ -1,11 +1,15 @@
 import random
+start = input('請決定隨機數字範圍開始值')
+end = input('請決定隨機數字範圍結束值')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
 	count = count + 1 #count += 1
 
-	num = input('請從1-100中猜一數字:')
+	num = input('請猜一數字:')
 	num = int(num)
 	if num == r:
 		print('終於猜對了!')
